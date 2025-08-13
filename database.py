47bdccfb6123315a95aeb53957c734af112d0b10
santiago garzon <santiago.garzon@nuvu.cc>
2025-08-13 14:20:41 -0500

    init fastapi base

--- database.py
+++ database.py
@@ -2,5 +2,5 @@ from pymongo import MongoClient
 from config import MONGO_URI
 
 client = MongoClient(MONGO_URI)
-db = client["Cluster0"]  
+db = client["Cluster0"]
 users_collection = db["Usuarios"]
